00ca46d035ac552c32151cf30f9089ea19ea89be
yatinranga <41532056+yatinranga@users.noreply.github.com>
2020-09-08 12:18:42 +0530

    created getMin and insert method for minimum prioroty queue

--- src/priority_queue/PriorityQueue.java
+++ src/priority_queue/PriorityQueue.java
@@ -10,12 +10,42 @@ public class PriorityQueue {
 		this.heap = new ArrayList<Integer>();
 	}
 
-	public boolean isEmpty() {
+	boolean isEmpty() {
 		return heap.size() == 0;
 	}
 
-	public int size() {
+	int size() {
 		return heap.size();
+	}
+
+	int getMin() throws PriorityQueueException {
+
+		if (heap.isEmpty()) {
+			throw new PriorityQueueException();
+		}
+
+		return heap.get(0);
+	}
+
+	void insert(int element) {
+
+		heap.add(element);
+		int childIndex = heap.size() - 1;
+		int parentIndex = (childIndex - 1) / 2;
+
+		while (childIndex > 0) {
+
+			if (heap.get(parentIndex) > heap.get(childIndex)) {
+				int temp = heap.get(parentIndex);
+				heap.set(parentIndex, heap.get(childIndex));
+				heap.set(childIndex, temp);
+				childIndex = parentIndex;
+				parentIndex = (childIndex - 1) / 2;
+			} else {
+				return;
+			}
+		}
+
 	}
 
 }
