21c0f334217083c6dc784e54e356a9e81b6bc371
yatinranga <41532056+yatinranga@users.noreply.github.com>
2020-09-21 02:07:06 +0530

    finding fibo without using recursion and using memoization technique

--- src/dp/Fibonacci1.java
+++ src/dp/Fibonacci1.java
@@ -2,6 +2,7 @@ package dp;
 
 public class Fibonacci1 {
 
+	// finding fibo using recursion only
 	public static int fibWithoutDP(int n) {
 		if (n == 0 || n == 1)
 			return n;
@@ -22,6 +23,7 @@ public class Fibonacci1 {
 		return fibArray[n];
 	}
 
+	// finding fibo using recursion and memoization technique
 	public static int fib(int n) {
 		int[] fibArray = new int[n + 1];
 		for (int i = 0; i < fibArray.length; i++) {
@@ -31,11 +33,27 @@ public class Fibonacci1 {
 
 	}
 
+	// finding fibo without using recursion and using memoization technique
+	public static int fibDP(int n) {
+
+		if (n == 0 || n == 1)
+			return n;
+		int storage[] = new int[n + 1];
+		storage[0] = 0;
+		storage[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			storage[i] = storage[i - 1] + storage[i - 2];
+		}
+		return storage[n];
+	}
+
 	public static void main(String[] args) {
 
 		int n = 45;
+		System.out.println(fibDP(n));
+
 		System.out.println(fib(n));
-		
+
 		// A lot of time is taken for result without using DP
 		System.out.println(fibWithoutDP(n));
 	}
