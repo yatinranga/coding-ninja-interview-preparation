2c6f0fbdcd5639d6a66260da9b601cd8115ab948
yatinranga <41532056+yatinranga@users.noreply.github.com>
2020-09-22 01:12:21 +0530

    finding min step to 1 using memoization technique and using bottom up approach

--- src/dp/MinStepToOne.java
+++ src/dp/MinStepToOne.java
@@ -68,10 +68,37 @@ public class MinStepToOne {
 		return countStepsusingMemoization(n, storage);
 	}
 
+	public static int countStepsDP(int n) {
+		int[] storage = new int[n + 1];
+		storage[1] = 0; // because we're using bottom to top approach
+
+		for (int i = 2; i <= n; i++) {
+			int min = storage[i - 1];
+
+			if (i % 3 == 0) {
+				if (min > storage[i / 3]) {
+					min = storage[i / 3];
+				}
+			}
+
+			if (i % 2 == 0) {
+				if (min > storage[i / 2]) {
+					min = storage[i / 2];
+				}
+			}
+
+			storage[i] = 1 + min;
+		}
+
+		return storage[n];
+
+	}
+
 	public static void main(String[] args) {
 
 		int n = 120;
 		System.out.println(countStepsusingMemoization(n));
+		System.out.println(countStepsDP(n));
 		System.out.println(countSteps(n));
 
 	}
